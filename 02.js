881e6306d18d846177e9e92205276edd45b06e76
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 00:52:08 +0530

    intro to variables

---
+++ 02.js
@@ -0,0 +1,21 @@
+"use strict";
+// intro to variables
+
+// variable can store some information 
+// we can use that information later 
+// we can change that information later
+
+
+// declare a variable 
+
+ var firstname = "hello";
+// use a variable 
+console.log(firstname);
+
+
+// change value
+
+firstname = "shadab";
+// console.log(firstName)
+
+console.log(firstname);
