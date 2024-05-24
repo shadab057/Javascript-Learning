e092fc919e07436c6d41e5ea3aaace0633f702fd
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 01:04:42 +0530

    rules for namimg variables

---
+++ 03.js
@@ -0,0 +1,39 @@
+  // rules for namimg variables
+
+//   we cannot start with number
+//  example :-
+
+
+// 1value (invalid)
+
+// var 1value = "hello";
+// console.log(1value);
+
+// value1 (valid)
+var value1 = "hello"
+console.log(value1);
+
+
+// you can use only undersore _ or dollar symbol 
+// first_name (valid)
+var  first_name = "abcd";
+console.log(first_name);
+
+// _firstname (valid) 
+
+var _firstname = "qweerty";
+console.log(_firstname);
+
+// first$name (valid)
+// $firstname (valid)
+
+// you cannot use spaces 
+// var first_name = "harshit"; // snake case writing 
+// var firstName = "harshit"; // camel case writing 
+// first name (invalid)
+
+// var first name = "thankyou";
+// console.log(first name);
+
+// convention 
+// start with small letter and use camelCase 
