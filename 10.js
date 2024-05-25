aa3492afbeb35e352e26bbfea0dd52b6ea81ffc4
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 18:46:06 +0530

    template string

---
+++ 10.js
@@ -0,0 +1,11 @@
+// template string
+
+let age = 22;
+let firstname = "shadab" ;
+
+// "my name is shadab and my age is 22"
+
+// let aboutme = "my name is " + firstname + " and my age is " + age;
+
+let aboutme = `my name is ${firstname} and my age is ${age}`
+console.log(aboutme);
