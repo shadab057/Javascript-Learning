0e6f3a8a531ec09a1c7ac19b6d7c09bbb7580d2a
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 18:38:15 +0530

    string concatenation

---
+++ 09.js
@@ -0,0 +1,17 @@
+// string concatenation 
+
+
+// let string1 = "shadab";
+// let string2 = "mohd";
+
+// let fullname = string1 + " "+ string2;
+// console.log(fullname);
+
+
+let string1 = "17";
+let string2 = "19";
+
+let fullname = +string1 + +string2 ;
+console.log(typeof fullname);
+
+
