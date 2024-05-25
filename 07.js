7367f7ccff62a2a5d1faac5811e14bcafccf1570
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 15:56:43 +0530

    Useful string methods

---
+++ 07.js
@@ -0,0 +1,34 @@
+// Useful string methods
+
+
+
+// trim() method 
+
+let firstname = "shadab";
+
+// console.log(firstname.length);
+// firstname = firstname.trim();
+// console.log(firstname)
+// console.log(firstname.length);
+
+
+// toUpperCase()
+
+// firstname = firstname.toUpperCase();
+// console.log(firstname);
+
+// tolowerCase()
+
+// firstname = firstname.toLowerCase();
+// console.log(firstname);
+
+
+// slice
+
+
+// start index 
+// end index
+
+let newstring = firstname.slice(0,4); //shad
+console.log(newstring);
+
