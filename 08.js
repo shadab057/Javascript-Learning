688d33b88d6089a967286fbeff7c4e6134d65988
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 17:56:33 +0530

    type of operator

---
+++ 08.js
@@ -0,0 +1,44 @@
+// type of operator 
+
+// data types (premitive data types)
+// -- string "shadab"
+// -- number 2,3,4
+//  -- boolean 
+// -- undefined
+//  -- null
+// -- BigInt  
+// -- symbol  
+
+// let age = 22;
+// let firstname = "shadab";
+// let obj = { key: "value" };
+// let bool = true;
+
+
+// // console.log(typeof age);
+// // console.log(typeof firstname);
+// // console.log(typeof "shad");
+// // console.log(typeof 2);
+// // console.log(typeof obj);
+// // console.log(typeof bool); 
+
+// // convert number to string
+// // console.log(typeof (age + ""))
+// age = age + "";
+// console.log(typeof age)
+
+// // convert string to number 
+
+// let mystr = +"54";
+// console.log(typeof mystr);
+
+// Second method 
+
+// let age = 10;
+// age = String(age);
+// console.log(typeof age);
+
+let age = "10";
+age = Number(age);
+console.log(typeof age);
+
