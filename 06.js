1d5f8208f18e18cfaab7c081f85340fb8e16f2ab
shadab057 <mohdshadab4629@gmail.com>
2024-05-25 12:34:50 +0530

    string indexing

---
+++ 06.js
@@ -0,0 +1,25 @@
+// string indexing
+
+
+let firstname = "shadab";
+
+
+// s  h  a  d  a  b
+// 0  1  2  3  4  5
+
+
+// console.log(firstname[3]);
+// length of string 
+// firstname.length
+
+
+// find the length
+
+
+
+// console.log(firstname.length);     // space is count in length 
+
+
+// find the last index
+
+console.log(firstname[firstname.length-1]);
