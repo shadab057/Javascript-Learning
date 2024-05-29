841c01ccf9bba3b8ad67f949e178d2c3f0e78e67
shadab057 <mohdshadab4629@gmail.com>
2024-05-29 18:00:06 +0530

    truthy and falsy values

--- 13.js
+++ 13.js
@@ -2,10 +2,63 @@
 
 // falsy values
 
+// let firstName = "";
+
+// if(firstName){
+//     console.log(firstName);
+// }
+// else{
+//     console.log("firstName kinda empty");
+// }
+
+
 // false 
 // " "
 // null 
-// undefined
-// 0
 
+// let firstName = null;
+
+// if(firstName){
+//     console.log(firstName);
+// }
+// else{
+//     console.log("firstName kinda empty");
+// }
+
+// undefined
+
+
+// let firstName;
+
+// if(firstName){
+//     console.log(firstName);
+// }
+// else{
+//     console.log("firstName kinda empty");
+// }
+
+
+// 0
+// let firstName = 0;
+
+// if(firstName){
+//     console.log(firstName);
+// }
+// else{
+//     console.log("firstName kinda empty");
+// }
+
+
+// truthy 
+//  "abc"
+//  1 , -1
+
+let firstName = "abc";
+
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("firstName kinda empty");
+}
 
