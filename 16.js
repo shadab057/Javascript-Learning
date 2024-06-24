50bf1e0740b4f91e1e4b4f6bca87c6bf2dd0153f
shadab057 <mohdshadab4629@gmail.com>
2024-06-24 13:03:46 +0530

    AND | OR Operator

---
+++ 16.js
@@ -0,0 +1,29 @@
+// and |  or operator 
+
+let firstName = "Shadab";
+let age = 17;
+
+// if(firstName[1] === "h"){
+//     console.log("your name starts with h");
+// }
+
+// if(age > 18) {
+//     console.log("you are above 18");
+// }
+
+
+//  And operator 
+// if (firstName[0] === "S" && age>=18) {
+//     console.log("Name starts with a and above 18");
+// }
+// else{
+//     console.log("inside else");
+// }
+
+// Or operator 
+if (firstName[0] === "S" || age>=18) {
+    console.log("inside if");
+}
+else{
+    console.log("inside else");
+}
