ca099538e151601355c130a9e44307383d3ea330
shadab057 <mohdshadab4629@gmail.com>
2024-06-26 00:58:21 +0530

    while loop

---
+++ 20.js
@@ -0,0 +1,33 @@
+//  while loop 
+
+// print  0 to 9
+
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+
+// dry don't repeat yourself 
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+
+
+let count = 1;
+
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
+
+console.log(`current value of count is ${count}`);
+console.log("hello");
