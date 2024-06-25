e620993631ce114e2f5ea9c6b0bf3a0d078a252e
shadab057 <mohdshadab4629@gmail.com>
2024-06-25 23:41:27 +0530

    if || else if || else conditions

---
+++ 18.js
@@ -0,0 +1,26 @@
+// if 
+// else if 
+// else if
+// else if
+// else 
+
+let tempDegree = 50;
+
+if(tempDegree < 0) {
+    console.log("extremely cold outside");
+}
+else if(tempDegree < 16){
+    console.log("It is cold outside");
+}
+else if (tempDegree < 25){
+    console.log("wheater is okay");
+}
+else if (tempDegree <35) {
+    console.log("lets go for swim");
+}
+else if (tempDegree < 45){
+    console.log("turn on AC")
+}
+else{
+    console.log("too hot")
+}
