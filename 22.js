748878c1b6d7ff0cbc3a620907c3b4e8d2688049
shadab057 <mohdshadab4629@gmail.com>
2024-06-27 19:03:25 +0530

    intro to for loop

---
+++ 22.js
@@ -0,0 +1,14 @@
+//  intro to for loop 
+//  print 0 to 9 
+
+
+// for(let i = 0; i< 10 ; i++){
+//     console.log(i)
+// }
+
+
+for(var i = 0; i< 10 ; i++){
+    console.log(i)
+}
+
+console.log("value of i is " ,i);
