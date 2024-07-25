2aa3557e96773036d70798d2308e752a2f89a862
shadab057 <mohdshadab4629@gmail.com>
2024-07-25 10:38:49 +0530

    Array in javascript

---
+++ 26.js
@@ -0,0 +1,24 @@
+//  intro to arrays
+//  reference  type 
+//  how to create arrays 
+
+// let fruit = "apple";
+
+// order collection of items
+
+
+// let fruits  = ["apple", "mango","grapes"];
+// let number = [1,2,3,4,5,6,7,8,9];
+// let mixed = [1,2,3, "mango",null, undefined];
+// console.log(mixed);
+// console.log(fruits[0]);  
+// console.log(number);
+
+
+// replace mango to another fruits 
+let fruits  = ["apple", "mango","grapes"];
+console.log(fruits);
+fruits [1] = "banana";
+console.log(fruits);
+
+//  array indexing 
