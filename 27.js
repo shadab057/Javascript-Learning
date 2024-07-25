ae7b8e746f365a0c84f226ec23c864403a006d60
shadab057 <mohdshadab4629@gmail.com>
2024-07-25 10:58:59 +0530

    push and pop  mehtod in array .

---
+++ 27.js
@@ -0,0 +1,30 @@
+// array push or pop
+
+
+// array shift unshift
+
+let fruits = ["apple","mango","grapes"];
+console.log(fruits);
+
+
+//  let's push something in last 
+// fruits.push("banana");
+// console.log(fruits);
+
+// add a element at the starting 
+// fruits.unshift("banana");
+// console.log(fruits);
+
+// push and pop  is fast as compared to a shift and unshift.
+
+//  pop the element 
+// let poppedfruit = fruits.pop();
+// console.log(fruits);
+// console.log("popped fruits is", poppedfruit);
+
+
+// pop the element at the starting
+
+let removedfruit = fruits.shift();
+console.log(fruits);
+console.log("removed fruits is ", removedfruit);
