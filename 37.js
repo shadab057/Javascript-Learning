f520b1f50386574b5d67d5b420d5fe5359cfba24
shadab057 <mohdshadab4629@gmail.com>
2024-08-11 17:54:07 +0530

    diff b/w dot and bracket square notation

---
+++ 37.js
@@ -0,0 +1,24 @@
+// difference between dot and bracket notation 
+
+// const person = {
+//     name: "shadab",
+//     age:22,
+//     hobbies:["guiter","sleeping","listening music"]
+// }
+
+// // by dot notation
+// console.log(person.hobbies);
+
+// // by square bracket notation
+// console.log(person[hobbies])
+
+
+const key = "email"
+const person = {
+    name: "shadab",
+    age:22,
+    hobbies:["guiter","sleeping","listening music"]
+}
+
+person["key] = "shadab@123gmail.com";
+console.log(person)
