55d48c822d73c3fac0eb7b7455519ea3e4a982e0
shadab057 <mohdshadab4629@gmail.com>
2024-08-15 17:12:29 +0530

    function in js

---
+++ 44.js
@@ -0,0 +1,36 @@
+//  function in js  
+function singHappyBirthday(){
+    console.log("Happy birthday to you .......");
+
+}
+
+
+function twoPlusFour(number1,number2){
+    // console.log(2+4);
+    // return 2+3;
+    return number1 + number2;
+}
+
+// twoPlusFour();
+// singHappyBirthday();
+// console.log(twoPlusFour());
+
+// const returnValue = twoPlusFour(4,9);
+// console.log(returnValue);
+
+
+// odd or even
+// input : 1 number
+
+// output : truw , false
+
+function isEven(number){
+    if (number % 2 === 0) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isEven(2))
