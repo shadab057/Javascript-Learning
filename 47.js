516125e245278626dbc6c695c92e4b362898b5a8
shadab057 <mohdshadab4629@gmail.com>
2024-08-17 16:53:41 +0530

    hoisting in javascript

---
+++ 47.js
@@ -0,0 +1,18 @@
+// hoisting 
+
+//  > hoisting allows you to use functions and variables before they're declared
+
+// hello();
+// function hello(){
+// console.log("hello world");
+// }
+
+// That is happen in only function declaration case
+
+
+// hello();
+// const hello = function(){
+//     console.log("hello world");
+// }
+
+// function expression 
