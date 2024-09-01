50ff5392cecc4d3c2e3abc480b0aa9f6aafdb914
shadab057 <mohdshadab4629@gmail.com>
2024-09-01 12:18:03 +0530

    fill method in js

---
+++ 64.js
@@ -0,0 +1,10 @@
+// fill method 
+// value , start , end
+
+
+// const myArr = new Array(10).fill(-1);
+// console.log(myArr);
+
+const myArray = [1,2,3,4,5,6,7,8];
+myArray.fill(0,2,5);
+console.log(myArray)
