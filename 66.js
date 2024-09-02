5133f4670932473b81da49aa06bcd1101fad0864
shadab057 <mohdshadab4629@gmail.com>
2024-09-02 13:11:43 +0530

    iterables

---
+++ 66.js
@@ -0,0 +1,19 @@
+// iterables
+
+// const firstName = "Harshit";
+// for(let char of firstName){
+// console.log(char);
+// }
+
+// const items = ['item', 'item2', 'item3'];
+//  for(let item of items){
+//         console.log(item);
+//     }
+
+// array like object
+
+    const firstName = "harshit";
+console.log(firstName.length);
+console.log(firstName[2]);
+
+
